1f3e94e8675f686576a9ba96c7c2d39601061530
daviddmr <david.dmr@gmail.com>
2020-03-18 13:16:10 -0400

    Implementing a single test as first sample

--- src/main/java/service/LocationService.java
+++ src/main/java/service/LocationService.java
@@ -3,6 +3,7 @@ package service;
 import entity.Location;
 import entity.Movie;
 import entity.User;
+import util.DataUtils;
 
 import java.util.Date;
 
@@ -10,7 +11,7 @@ import static util.DataUtils.addDays;
 
 public class LocationService {
 
-    public Location rentMovie(User user, Movie movie) {
+    private Location rentMovie(User user, Movie movie) {
         Location location = new Location();
         location.setMovie(movie);
         location.setUser(user);
@@ -29,6 +30,18 @@ public class LocationService {
     }
 
     public static void main(String[] args) {
+        //scenario
+        LocationService locationService = new LocationService();
+        User user = new User("");
+        Movie movie = new Movie("Movie 1", 2, 5.0);
+
+        //action
+        Location location = locationService.rentMovie(user, movie);
+
+        //verification
+        System.out.println(location.getValue() == 5.0);
+        System.out.println(DataUtils.isSameDate(location.getLocationDate(), new Date()));
+        System.out.println(DataUtils.isSameDate(location.getReturnDate(), DataUtils.getDifferenceBetweenToDays(1)));
 
     }
 }
