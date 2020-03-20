2cb339b06e1b3a9c37c78922b51a2e06c5a24670
daviddmr <david.dmr@gmail.com>
2020-03-19 20:40:31 -0400

    Create a new parameter to clarify the identification of each test

--- src/test/java/service/CalculationRentValueTest.java
+++ src/test/java/service/CalculationRentValueTest.java
@@ -27,6 +27,9 @@ public class CalculationRentValueTest {
     @Parameterized.Parameter(value = 1)
     public Double locationAmount;
 
+    @Parameterized.Parameter(value = 2)
+    public String scenario;
+
     @Before
     public void setup() {
         locationService = new LocationService();
@@ -40,14 +43,14 @@ public class CalculationRentValueTest {
     private static Movie movie6 = new Movie("Movie 6", 2, 4.0);
     private static Movie movie7 = new Movie("Movie 6", 2, 4.0);
 
-    @Parameterized.Parameters
+    @Parameterized.Parameters(name = "{2}")
     public static Collection<Object[]> getParameters() {
         return Arrays.asList(new Object[][]{
-                {Arrays.asList(movie1, movie2, movie3), 11.0},
-                {Arrays.asList(movie1, movie2, movie3, movie4), 13.0},
-                {Arrays.asList(movie1, movie2, movie3, movie4, movie5), 14.0},
-                {Arrays.asList(movie1, movie2, movie3, movie4, movie5, movie6), 14.0},
-                {Arrays.asList(movie1, movie2, movie3, movie4, movie5, movie6, movie7), 18.0}
+                {Arrays.asList(movie1, movie2, movie3), 11.0, "3 Movies - 25%"},
+                {Arrays.asList(movie1, movie2, movie3, movie4), 13.0, "4 Movies - 50%"},
+                {Arrays.asList(movie1, movie2, movie3, movie4, movie5), 14.0, "5 Movies - 75%"},
+                {Arrays.asList(movie1, movie2, movie3, movie4, movie5, movie6), 14.0, "6 Movies - 100%"},
+                {Arrays.asList(movie1, movie2, movie3, movie4, movie5, movie6, movie7), 18.0, "7 Movies - No discount"}
         });
     }
 
