00d833d3cb62ef03a2aa8838edb3864fb2a06ec5
daviddmr <david.dmr@gmail.com>
2020-03-21 21:27:59 -0400

    Create business logic for WeekDayMatcher

--- src/test/java/matchers/WeekDayMatcher.java
+++ src/test/java/matchers/WeekDayMatcher.java
@@ -2,16 +2,20 @@ package matchers;
 
 import org.hamcrest.Description;
 import org.hamcrest.TypeSafeMatcher;
+import utils.DataUtils;
 
 import java.util.Date;
 
 public class WeekDayMatcher extends TypeSafeMatcher<Date> {
 
+    private Integer weekDay;
+
     public WeekDayMatcher(Integer weekDay) {
+        this.weekDay = weekDay;
     }
 
     protected boolean matchesSafely(Date date) {
-        return false;
+        return DataUtils.checkWeekDay(date, weekDay);
     }
 
     public void describeTo(Description description) {
